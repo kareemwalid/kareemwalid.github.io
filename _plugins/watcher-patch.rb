058ea33538d75c499e6d749812724faa1fa45401
kareemwalid <kareemwalid063@gmail.com>
2023-12-04 21:27:07 +0200

    customizing my personal blog

--- _plugins/watcher-patch.rb
+++ _plugins/watcher-patch.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
-require 'jekyll-watch'
-module jekyll
-    module Watcher
-        extend self
 
-        alias_method :original_listen_ignore_paths, :listen_ignore_paths
-        
-        def listen_ignore_paths(options)
-            original_listen_ignore_paths(options) + (%r!.*\.TMP!i)
-        end
+require 'jekyll-watch'
+
+module Jekyll
+  module Watcher
+    extend self
+
+    alias_method :original_listen_ignore_paths, :listen_ignore_paths
+
+    def listen_ignore_paths(options)
+        original_listen_ignore_paths(options) + [%r!.*\.TMP!i]
     end
+  end
 end
